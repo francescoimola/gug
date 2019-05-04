879c3bb679c43a5b9cf10465e9e527f078c441cd
francescoimola <imolafrancesco.mailbox@gmail.com>
2019-05-04 23:00:05 +0100

    Final Working version (no design yet)

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-data-gramm="false"
-
 //Define list of emojis
 let emoji = []
 emoji = JSON.parse(JSON.stringify(EmojiListJson))
 const emojiLenght = Object.keys(emoji).length
 
-let nicknames = ["lad", "bloke", "man", "guy", "boy", "hung", "slut", "bitch", "whore", "angel", "brit", "slave", "daddy", "asian", "american", "bubble", "Honey", "otter", "dad", "twink", "bear", "hun", "chub", "jock", "chaser"]
-let adj = ["fuzzy", "hairy", "discret", "dirty", "clean", "adventurous", "caring", "shaven", "smooth", "thicc", "thick", "slim", "sugar", "filthy", "bored", "horny", "flexible"]
+let nicknames = ["lad", "bloke", "man", "guy", "boy", "hung", "slut", "bitch", "whore", "angel", "brit", "slave", "daddy", "asian", "american", "bubble", "Honey", "otter", "dad", "twink", "bear", "hun", "chub", "jock", "chaser", "qween", "queen"]
+let adj = ["fuzzy", "hairy", "discret", "dirty", "clean", "adventurous", "caring", "shaven", "smooth", "thicc", "thick", "slim", "sugar", "filthy", "bored", "horny", "flexible", "skinny"]
 let time = ["TODAY", "TONIGHT", "THIS EVENING", "Meet later", "Right now"]
 let topBottom = ["top", "tp", "4top", "bottom", "bttm", "4bttm"]
 let hey = ["hey", "heya", "hello", "hi", "Yas"]
 let initials = ["G", "F", "T", "M", "H"]
-let locations = ["New to the area", "local"]
+let locations = ["new to the area", "local"]
 let accom = ["can accom", "can accomodate", "cant accom", "cant accomodate", "ACCOM", "NO ACCOM"]
 let preMade = ["mouth 4 use", "HnH", "Head", "BDSM", "Maso", "Use me", "Masc", "masc4masc", "Femme", "Femm", "Pop me", "Huge", "Anyone", "Hi hun", "Honey", "Guy next door", "Boy next door"]
 let signs = ["!", "?"]
-
-//Divide Emojis for Category
-// const smileysEmotion = emoji.slice(0, 150)
-// const peopleBodies = emoji.slice(150, 461)
-// const animalsNature = emoji.slice(465, 592)
-// const foodDrink = emoji.slice(592, 713)
-// const travelPlaces = emoji.slice(713, 922)
-// const activities = emoji.slice(922, 1002)
-// const objects = emoji.slice(1002, 1235)
-// const symbols = emoji.slice(1235, 1452)
-// const flags = emoji.slice(1452, 1720)
-
 
 function myFunction(a) {
 
@@ -67,30 +53,56 @@ let myArray = []
 myArray[0] = myFunction(emoji)
 myArray[1] = myFunction(accom)
 myArray[3] = myFunction(nicknames)
-myArray[5] = myFunction(adj)
-myArray[6] = myFunction(time)
-myArray[7] = myFunction(topBottom)
-myArray[8] = myFunction(hey)
-myArray[9] = myFunction(initials)
-myArray[10] = myFunction(locations)
+myArray[4] = myFunction(adj)
+myArray[5] = myFunction(time)
+myArray[6] = myFunction(topBottom)
+myArray[7] = myFunction(hey)
+myArray[8] = myFunction(initials)
+myArray[9] = myFunction(locations)
+myArray[10] = myFunction(preMade)
+myArray[11] = myFunction(signs)
 
 //Remove undefined values
-for (i = 0; i <= myArray.Lenght; i++){
-   function removeEmpty(obj) {
-      Object.keys(obj).forEach(function(key) {
-        (obj[key] && typeof obj[key] === 'object') && removeEmpty(obj[key]) ||
-        (obj[key] === undefined || obj[key] === null) && delete obj[key]
+function removeEmpty(obj) {
+      Object.keys(obj).forEach(function (key) {
+         (obj[key] && typeof obj[key] === 'object') && removeEmpty(obj[key]) ||
+            (obj[key] === undefined || obj[key] === null) && delete obj[key]
       });
       return obj;
-   }
-   removeEmpty(myArray[i])
-   if (myArray[i] == undefined) {
-      removeEmpty(myArray[i])
+}
+
+function checkArray(a) {
+   for (i = 0; i <= a.lenght; i++) {
+      removeEmpty(a[i])
+      if (a[i] == undefined) {
+         removeEmpty(a[i])
+      }
+   } 
+}
+
+checkArray(myArray)
+
+var printThis = "Hello World"
+
+//Logic
+function hello(a) {
+   var random_boolean = Math.random() >= 0.5;
+   var val1 = a[Math.floor(Math.random() * Math.floor(a.length))]
+   var val2 = a[Math.floor(Math.random() * Math.floor(a.length))] + " " + a[Math.floor(Math.random() * Math.floor(a.length))]
+   if (random_boolean && val1 !== undefined) {
+      checkArray(val1)
+      return printThis = val1
+   } else if (random_boolean !== true && val2 !== undefined) {
+      checkArray(val2)
+      return printThis = val2
    }
 }
 
-
-console.log(myArray[0] + " " + myArray[6])
+console.log(hello(myArray))
 
 //Display output on page
-// document.getElementById("scriptOutput").innerHTML = something
+
+if (hello) {
+   var msg = printThis.replace(/undefined/g, "");
+   document.getElementById("scriptOutput").innerHTML = msg
+}
